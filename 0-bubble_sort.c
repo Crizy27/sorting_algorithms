0026915828262d5efcd2b3a3b2a704290b8f5ae9
Crizy27 <crisssamuelserunjogi@gmail.com>
2023-01-22 19:31:19 +0000

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,47 +1,48 @@
 #include "sort.h"
 
 /**
- * swap_values - swaps 2 values in an array of ints
- *
- * @array: the array of ints
- * @i1: index of first value
- * @i2: index of 2nd value
- *
- * Return: the array with value
+ * swap_bubble - function for sorting with swap method.
+ *@a: element to be swapped.
+ *@b: element to be swapped.
+ * Return: nothing.
  */
 
-void swap_values(int **array, ssize_t i1, ssize_t i2)
+void swap_bubble(int *a, int *b)
 {
-	int tmp;
+	int temp;
 
-	tmp = (*array)[i1];
-	(*array)[i1] = (*array)[i2];
-	(*array)[i2] = tmp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * bubble_sort - sorts an array of integers using bubble sort
- *
- * @array: the array of integers
- * @size: the size of the array
+ * bubble_sort - sorts an array using bubble sort algorithm.
+ *@array: array to be sorted.
+ *@size: size to be sorted.
+ * Return: void.
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t n = size, new_n, i;
+	size_t i, sz = size;
+	bool booln = false;
 
-	while (n > 1)
+	if (array == NULL || size < 2)
+		return;
+
+	while (booln == false)
 	{
-		new_n = 0;
-		for (i = 1; i <= n - 1; i++)
+		booln = true;
+		for (i = 0; i < sz - 1; i++)
 		{
-			if (array[i - 1] > array[i])
+			if (array[i] > array[i + 1])
 			{
-				swap_values(&array, i - 1, i);
+				swap_bubble(array + i, array + i + 1);
 				print_array(array, size);
-				new_n = i;
+				booln = false;
 			}
 		}
-		n = new_n;
+		sz--;
 	}
 }
--- sort.h
+++ sort.h
@@ -3,8 +3,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
+/**
+ * enum bool - struct for bool type
+ *@false: false is equivalent to 0
+ *@true: equivalent to 1
+ * Return: nothing
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 
 /**
  * struct listint_s - Doubly linked list node
@@ -20,24 +30,37 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* UTILS */
+
+/* aux functions */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_bubble(int *a, int *b);
+int partition(int *array, size_t size, int low, int high);
+void partition_sort(int *array, size_t size, int low, int high);
+void merge_sort_call(int *subarray, int *buffer, size_t lo, size_t hi);
+void merge_sub(int *subarray, int *buffer, size_t lo, size_t mid, size_t hi);
+void partition_sort_hoare(int *array, size_t size, int low, int high);
+int partition_hoare(int *array, size_t size, int low, int high);
+void swap_tail(listint_t **list, listint_t **head, listint_t **tail);
+void swap_head(listint_t **list, listint_t **tail, listint_t **head);
+int get_max(int *array, int size);
+void lsd_sort(int *array, size_t size, int exp, int *copy);
 
-/* SORT ALGORITHMS */
+
+
+/* algorithm functions */
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-/* ADVANCED SORT ALGORITHMS */
 void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void radix_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
 
-#endif /* SORT_H */
+
+#endif
